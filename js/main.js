c0d99ce3cf736d616b20ce99a13b7893eba7830e
Aziz Abduhakimov <byfuzayliy@gmail.com>
2025-06-03 22:38:53 +0500

    Added delete function and fixed some bug

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let selectLevel = document.querySelector("#taskLevel");
 let taskForm = document.querySelector(".task-form");
 let taskmodal = document.querySelector(".task-modal");
 let tasksTable = document.querySelector(".tasks-table tbody");
+let openModalBtn = document.querySelector(".open-modal-btn");
 let tasksJson = localStorage.getItem("tasks");
 let tasks = JSON.parse(tasksJson) || [];
 filterLevel.innerHTML += `<option>All</option>`;
@@ -24,19 +25,18 @@ taskForm.addEventListener("submit", function (e) {
     tasks.push(task);
     localStorage.setItem("tasks", JSON.stringify(tasks));
     bootstrap.Modal.getInstance(taskmodal).hide();
+    this.classList.remove("was-validated");
+
     getTasks();
   }
 });
 
 function getTaskCard({ taskName, taskTime, taskLevel }, i) {
-  console.log(taskLevel);
-
   let levelColor = {
     Low: "info",
     Middle: "warning",
     High: "danger",
   };
-  console.log(levelColor);
 
   return `<tr>
   <th scope="row">${i + 1}</th>
@@ -46,10 +46,10 @@ function getTaskCard({ taskName, taskTime, taskLevel }, i) {
       <span  class="badge bg-${levelColor[taskLevel]}">${taskTime}</span>
     </div>
     <div class="d-flex gap-2 mt-2 w-100">
-      <button class="btn btn-primary btn-sm w-50">
+      <button  onClick="editTask(${i})" data-bs-toggle="modal" data-bs-target="#task-modal" class="btn btn-primary btn-sm w-50">
         <img src="./assets/image/edit.png" alt="Edit" />
       </button>
-      <button class="btn btn-danger btn-sm w-50">
+      <button onClick="deleteTask(${i})" class="btn btn-danger btn-sm w-50">
         <img src="./assets/image/bin.png" alt="Delete" />
       </button>
     </div>
@@ -67,3 +67,29 @@ function getTasks() {
 }
 
 getTasks();
+
+const deleteTask = (id) => {
+  console.log(id);
+
+  let check = confirm("Do you want to delete this task ?");
+  if (check) {
+    tasks.splice(id, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    getTasks();
+  }
+};
+
+const editTask = (id) => {
+  const { taskName, taskTime, taskLevel } = tasks[id];
+  taskForm.taskName.value = taskName;
+  taskForm.taskTime.value = taskTime;
+  taskForm.taskLevel.value = taskLevel;
+};
+
+openModalBtn.addEventListener("click", () => {
+  const { taskName, taskTime, taskLevel } = taskForm.elements;
+
+  taskName.value = "";
+  taskTime.value = "";
+  taskLevel.value = levels[0];
+});
